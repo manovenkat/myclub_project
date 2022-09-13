01eb58bb9d663ef97e7e70768d4eab3c61d9766c
manovenkata <mvenkatachalam@planoconsulting.net>
2022-09-13 06:00:17 -0500

    changes pushed

--- myclub_root/events/views.py
+++ myclub_root/events/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
 from datetime import date
 import calendar
 from calendar import HTMLCalendar
 
 from .models import Event
+from .forms import VenueForm
 
-def index2(request):
-    t = date.today()
-    month = date.strftime(t, '%b')
-    year = t.year
-    title = "MyClub Event Calendar - %s %s" % (month, year)
-    # return HttpResponse("<h1>%s</h1>" % title)
-    return HttpResponse("<h1>title</h1>")
 
 def index(request, year=date.today().year, month=date.today().month):
     # t = date.today()
@@ -47,3 +43,20 @@ def all_events(request):
         'events/event_list.html',
         {'event_list': event_list}
     )
+
+
+def add_venue(request):
+    submitted = False
+    if request.method == 'POST':
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue/?submitted=True')
+    else:
+        form = VenueForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 
+        'events/add_venue.html', 
+        {'form': form, 'submitted': submitted}
+        )
